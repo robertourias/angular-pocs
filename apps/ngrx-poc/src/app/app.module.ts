63b436d1adcacbfb36ca45a05afb33f3f6482a4e
Roberto Nicoletti <roberto.urias@gmail.com>
2020-09-30 15:10:12 -0300

    NgRx Poc: Criando components

--- apps/ngrx-poc/src/app/app.module.ts
+++ apps/ngrx-poc/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { cartReducer } from './reducers/cart.reducer';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductListComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+  ],
   imports: [
     BrowserModule,
     StoreModule.forRoot(
-      {},
+      {
+        cart: cartReducer
+      },
       {
         metaReducers: !environment.production ? [] : [],
         runtimeChecks: {
@@ -23,8 +40,16 @@ import { environment } from '../environments/environment';
     ),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    IonicModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+      }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
