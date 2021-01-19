7218635b2841ba42a1be4e7653d4bd6b4b042a3f
Roberto Nicoletti <roberto.urias@gmail.com>
2021-01-18 23:00:08 -0300

    add produtos e ngrx

--- apps/ngrx/src/app/app.component.ts
+++ apps/ngrx/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angular-pocs-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngrx';
-}
+export class AppComponent {}
--- apps/ngrx/src/app/app.module.ts
+++ apps/ngrx/src/app/app.module.ts
@@ -1,12 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CouterComponent } from './couter/couter.component';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './products/products.module';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProductsComponent } from './products/containers/products/products.component';
+
+import { environment } from '../environments/environment';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  },
+  { path: '**',
+    redirectTo: '/products'
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, CouterComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    ProductsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
